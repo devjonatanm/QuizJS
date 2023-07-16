b88460ff96174a8ec642178d6a53fd1abcb6f499
Jonatan Marques <jonatanmarsv@gmail.com>
2023-07-16 20:10:49 -0300

    versão 2.0 finalizada logica com arrays

--- app.js
+++ app.js
@@ -3,29 +3,68 @@
 
 const div = document.querySelector('div.perguntas')
 
+const quiz = [
+    {
+        pergunta: 'Um aplicativo construído em linguagens da web, tais como HTML, CSS e Javascript é um aplicativo:',
+        resposta: ['Original.', 'Híbrido.', 'Padrão.', 'Nativo.'],
+        correta: 'p2'
+    },
+    {
+        pergunta: 'O Ajax utiliza algumas tecnologias bastante conhecidas com o objetivo de melhorar a interatividade entre o usuário e o servidor. A tecnologia utilizada pelo Ajax que, na verdade, é um objeto JavaScript que torna possível a comunicação assíncrona com o servidor, sem a necessidade de recarregar a página por completo, é:',
+        resposta: ['CSS', 'XML', 'DOM', 'XMLHttpRequest'],
+        correta: 'p4'
+    },
+    {
+        pergunta: 'JavaScript is a _____ Side Scripting Language.',
+        resposta: ['Server', 'Browser', 'ISP', 'None of the above'],
+        correta: 'p2'
+    },
+    {
+        pergunta: 'o código JavaScript é escrito dentro do arquivo com extensão',
+        resposta: ['.jvs', ' .js', '.jsc', '.javascript'],
+        correta: 'p2'
+    },
+    {
+        pergunta: 'Local Browser used for validations on the Web Pages uses',
+        resposta: ['JS', 'Java', 'HTML', 'CSS'],
+        correta: 'p1'
+    }
+]
+
+let count = 0
+let pontos = 0
+
 document.querySelector('#iniciar').addEventListener('click', event => {
-    if (document.querySelector('button').getAttribute('id') === 'next') {
-        if(document.querySelectorAll('input')[0].checked === true){
-            console.log('acertou!')
-        }else{
-            console.log('errou!')
+
+    for (let i = 0; i < 4; i++) {
+        if (document.querySelector('button').getAttribute('id') === 'next' && quiz.length >= count) {
+            if (document.querySelectorAll('input')[i].checked === true && document.querySelectorAll('input')[i].getAttribute('id') === quiz[count - 1].correta) {
+                pontos++
+            }
         }
     }
 
     div.innerHTML = ''
-    div.innerHTML = `
-        <h3>How do you write "Hello World" in an alert box?</h3>
-        <p><input type="radio"" name="p" id="p1"> alert("Hello World")</p>
-        <p><input type="radio"" name="p" id="p2"> msgBox("Hello World")</p>
-        <p><input type="radio"" name="p" id="p3"> msg("Hello World")</p>
-        <p><input type="radio"" name="p" id="p4"> alertBox("Hello World")</p>
-        
-    `
-    if (document.querySelector('button').getAttribute('id') === 'iniciar') {
+
+    if (quiz.length > count) {
+        div.innerHTML = `
+        <h3>${count+1}/${quiz.length} ${quiz[count].pergunta}</h3>
+        <p><input type="radio"" name="p" id="p1"> ${quiz[count].resposta[0]}</p>
+        <p><input type="radio"" name="p" id="p2"> ${quiz[count].resposta[1]}</p>
+        <p><input type="radio"" name="p" id="p3"> ${quiz[count].resposta[2]}</p>
+        <p><input type="radio"" name="p" id="p4"> ${quiz[count].resposta[3]}</p>
+        `
+    } else {
+        div.innerHTML = `
+        <p>FINALIZADO VOCE ACERTOU ${pontos}</p>
+        `
+    }
+
+    if (document.querySelector('button').getAttribute('id') === 'iniciar' && quiz.length > count) {
         document.querySelector('#iniciar').setAttribute("id", "next")
         document.querySelector('button').textContent = 'Next'
     }
 
-    
 
+    count++
 })
